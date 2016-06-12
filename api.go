99d061348a83382b3325f873ef936b175ad47ff9
Filip Lindvall <drpytho@gmail.com>
2016-06-12 16:55:31 +0200

    Added handler function to take in form submitions

---
+++ api.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func GetFormHandler(m MailGun) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//TODO add spam filter here
+
+		decoder := json.NewDecoder(r.Body)
+		var data map[string]string
+		if err := decoder.Decode(data); err != nil {
+			// Error while parsing
+			log.Println("Error while parsing requests body")
+			// Maybe print som details
+			log.Print("Error from JSON encoder \n ", err.Error, "\n\n", "Body: \n", decoder.Buffered())
+			return
+		}
+		//TODO: Escape user data
+		s := CreateSimpleMail()
+		for k := range data {
+			if k == "email" || k == "Email" {
+				s.SetHeader("From", data[k])
+				s.SetHeader("Reply-To", data[k])
+			}
+		}
+		s.SetHeader("MIME-Version", "1.0")
+		s.SetHeader("Content-Type", "text/html")
+		bodyMsg := "<html><body>"
+		//Build message here
+		bodyMsg += "<h3>User message</h3>"
+		for k, v := range data {
+			bodyMsg += "<b>" + k + "</b>: " + v + "<br><hr>\n"
+		}
+		bodyMsg += "</body></html>"
+		s.SetBody(bodyMsg)
+		m.SendMail(s)
+	}
+}
